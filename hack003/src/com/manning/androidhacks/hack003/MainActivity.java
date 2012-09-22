34aa535997f5face01663fc8a10ea1b37b548eb8
Macarse <macarse@gmail.com>
2012-09-22 11:33:34 -0300

    Hack003

---
+++ hack003/src/com/manning/androidhacks/hack003/MainActivity.java
@@ -0,0 +1,30 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Manning
+ * See the file license.txt for copying permission.
+ ******************************************************************************/
+package com.manning.androidhacks.hack003;
+
+import android.os.Bundle;
+import android.view.View;
+import com.google.android.maps.MapActivity;
+
+public class MainActivity extends MapActivity {
+
+  private View mViewStub;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+    mViewStub = findViewById(R.id.map_stub);
+  }
+
+  public void onShowMap(View v) {
+    mViewStub.setVisibility(View.VISIBLE);
+  }
+
+  @Override
+  protected boolean isRouteDisplayed() {
+    return false;
+  }
+}
