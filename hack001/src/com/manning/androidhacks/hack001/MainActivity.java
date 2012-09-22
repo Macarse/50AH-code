04bb60b55df0d53f3c4688b43d6de37748e7cd2f
Macarse <macarse@gmail.com>
2012-09-22 10:54:26 -0300

    Hack001

---
+++ hack001/src/com/manning/androidhacks/hack001/MainActivity.java
@@ -0,0 +1,17 @@
+/*******************************************************************************
+ * Copyright (c) 2012 Manning
+ * See the file license.txt for copying permission.
+ ******************************************************************************/
+package com.manning.androidhacks.hack001;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.main);
+  }
+}
