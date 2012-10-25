001d40715c4197ff307d2ed6fb7499b9f4635073
Macarse <macarse@gmail.com>
2012-10-25 15:19:16 -0300

    Hack003 merged with hack002

--- hack002/src/com/manning/androidhacks/hack002/MainActivity.java
+++ hack002/src/com/manning/androidhacks/hack002/MainActivity.java
@@ -4,14 +4,27 @@
  ******************************************************************************/
 package com.manning.androidhacks.hack002;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import com.google.android.maps.MapActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends MapActivity {
+
+  private View mViewStub;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main);
+    mViewStub = findViewById(R.id.map_stub);
+  }
+
+  public void onShowMap(View v) {
+    mViewStub.setVisibility(View.VISIBLE);
+  }
+
+  @Override
+  protected boolean isRouteDisplayed() {
+    return false;
   }
 }
